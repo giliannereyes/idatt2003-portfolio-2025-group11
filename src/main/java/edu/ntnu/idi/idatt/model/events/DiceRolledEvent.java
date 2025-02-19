0feb5a11fde8b66b175009fca7f71c7f9fa9cdbb
giliannereyes <giliannekatereyes@yahoo.com>
2025-02-19 14:04:34 +0100

    feat: add game events and listener classes

---
+++ src/main/java/edu/ntnu/idi/idatt/model/events/DiceRolledEvent.java
@@ -0,0 +1,42 @@
+package edu.ntnu.idi.idatt.model.events;
+
+import edu.ntnu.idi.idatt.model.entities.Player;
+
+/**
+ * Represents an event where a player rolls the dice.
+ *
+ * @version 0.1
+ * @since 0.1
+ * @author Gilianne Reyes
+ */
+public class DiceRolledEvent implements GameEvent{
+    private final Player player;
+    private final int roll;
+
+    /**
+     * Constructs a DiceRolledEvent instance.
+     *
+     * @param player is the player that rolled the dice.
+     * @param roll is the number rolled on the dice.
+     */
+    public DiceRolledEvent(Player player, int roll) {
+        this.player = player;
+        this.roll = roll;
+    }
+
+    /**
+     * Gets the player that rolled the dice.
+     *
+     * @return the player that rolled the dice.
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    /**
+     * Gets the number rolled on the dice.
+     */
+    public int getRoll() {
+        return roll;
+    }
+}
---
+++ src/main/java/edu/ntnu/idi/idatt/model/events/PlayerMovedEvent.java
@@ -0,0 +1,56 @@
+package edu.ntnu.idi.idatt.model.events;
+
+import edu.ntnu.idi.idatt.model.entities.Player;
+
+/**
+ * Represents an event where a player moves from one tile to another.
+ *
+ * @version 0.1
+ * @since 0.1
+ * @author Gilianne Reyes
+ */
+public class PlayerMovedEvent {
+    private final Player player;
+    private final int fromTileId;
+    private final int toTileId;
+
+    /**
+     * Constructs a PlayerMovedEvent instance.
+     *
+     * @param player is the player that moved.
+     * @param fromTileId is the id of the tile the player moved from.
+     * @param toTileId is the id of the tile the player moved to.
+     */
+    public PlayerMovedEvent(Player player, int fromTileId, int toTileId) {
+        this.player = player;
+        this.fromTileId = fromTileId;
+        this.toTileId = toTileId;
+    }
+
+    /**
+     * Gets the player that moved.
+     *
+     * @return the player that moved.
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    /**
+     * Gets the id of the tile the player moved from.
+     *
+     * @return id of the tile the player moved from.
+     */
+    public int getFromTileId() {
+        return fromTileId;
+    }
+
+    /**
+     * Gets the id of the tile the player moved to.
+     *
+     * @return id of the tile the player moved to.
+     */
+    public int getToTileId() {
+        return toTileId;
+    }
+}
