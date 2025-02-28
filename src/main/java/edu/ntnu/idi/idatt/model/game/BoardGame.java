fe87cfb438eb14ce3548703e3670c224b4e7e156
giliannereyes <giliannekatereyes@yahoo.com>
2025-02-28 15:56:30 +0100

    refactor: update BoardGame + BoardGameTest

--- src/main/java/edu/ntnu/idi/idatt/model/game/BoardGame.java
+++ src/main/java/edu/ntnu/idi/idatt/model/game/BoardGame.java
@@ -5,17 +5,15 @@ import edu.ntnu.idi.idatt.model.entities.Dice;
 import edu.ntnu.idi.idatt.model.entities.Player;
 import edu.ntnu.idi.idatt.model.events.GameEvent;
 import edu.ntnu.idi.idatt.model.events.GameEventListener;
+import edu.ntnu.idi.idatt.utils.Validation;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Abstract class for board games. Contains a board, a list of players, a dice, and a winner.
  * Provides methods for setting up and playing the game.
  *
- * @version 0.1
+ * @version 0.2
  * @since 0.1
  * @author Gilianne Reyes
  */
@@ -33,8 +31,13 @@ public abstract class BoardGame {
      * @param board is the board of the game.
      * @param players is the list of players in the game.
      * @param dice is the dice used in the game.
+     *
+     * @throws IllegalArgumentException if any of the parameters is null.
      */
     public BoardGame(Board board, List<Player> players, Dice dice) {
+        Validation.validateNonNull(board, "Board");
+        Validation.validateNonNull(players, "Players");
+        Validation.validateNonNull(dice, "Dice");
         this.board = board;
         this.players = players;
         this.dice = dice;
@@ -70,8 +73,12 @@ public abstract class BoardGame {
      * @param eventType is the type of event to trigger.
      * @param listener is the listener to register.
      * @param <T> is the type of event.
+     *
+     * @throws IllegalArgumentException if any of the parameters is null.
      */
     public <T extends GameEvent> void addEventListener(Class<T> eventType, GameEventListener listener) {
+        Validation.validateNonNull(eventType, "Event type");
+        Validation.validateNonNull(listener, "Listener");
         eventListeners.computeIfAbsent(eventType, k -> new ArrayList<>()).add(listener);
     }
 
@@ -81,17 +88,27 @@ public abstract class BoardGame {
      * @param eventType is the type of event of the listener to unregister.
      * @param listener is the listener to unregister.
      * @param <T> is the type of event.
+     *
+     * @throws IllegalArgumentException if any of the parameters is null.
      */
     public <T extends GameEvent> void removeEventListener(Class<T> eventType, GameEventListener listener) {
+        Validation.validateNonNull(eventType, "Event type");
+        Validation.validateNonNull(listener, "Listener");
         eventListeners.get(eventType).remove(listener);
+        if (eventListeners.get(eventType).isEmpty()) {
+            eventListeners.remove(eventType);
+        }
     }
 
     /**
      * Notifies all listeners registered for a specific event type.
      *
      * @param event is the event to notify listeners of.
+     *
+     * @throws IllegalArgumentException if the event is null.
      */
     protected void notifyListeners(GameEvent event) {
+        Validation.validateNonNull(event, "Event");
         List<GameEventListener> listeners = eventListeners.get(event.getClass());
         if (listeners != null) {
             for (GameEventListener listener : listeners) {
@@ -117,6 +134,24 @@ public abstract class BoardGame {
      */
     protected int rollDice() {
         return dice.roll();
+    }
+
+    /**
+     * Retrieves the winner of the game.
+     *
+     * @return the winner of the game.
+     */
+    protected Player getWinner() {
+        return winner;
+    }
+
+    /**
+     * Retrieves the event listeners.
+     *
+     * @return the event listeners.
+     */
+    protected Map<Class<? extends GameEvent>, List<GameEventListener>> getEventListeners() {
+        return Collections.unmodifiableMap(eventListeners);
     }
 
     /**
