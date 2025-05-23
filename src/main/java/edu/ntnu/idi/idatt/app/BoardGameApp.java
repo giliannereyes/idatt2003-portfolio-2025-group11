aebbf5a1472ddd30af21f4315241596be59f1dff
giliannereyes <giliannekatereyes@yahoo.com>
2025-05-23 07:22:54 +0200

    docs: BoardGameApp

--- src/main/java/edu/ntnu/idi/idatt/app/BoardGameApp.java
+++ src/main/java/edu/ntnu/idi/idatt/app/BoardGameApp.java
@@ -1,16 +1,36 @@
 package edu.ntnu.idi.idatt.app;
 
 import edu.ntnu.idi.idatt.config.initializer.AppInitializer;
-import edu.ntnu.idi.idatt.utils.ViewManager;
 import edu.ntnu.idi.idatt.ui.view.GameSelectionView;
+import edu.ntnu.idi.idatt.utils.ViewManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * The main application class for the Board Game application.
+ *
+ * <p>This class initializes the JavaFX application and sets up the initial view.</p>
+ *
+ * @version 0.1
+ * @since 0.1
+ * @author Gilianne Reyes
+ */
 public class BoardGameApp extends Application {
+  /**
+   * The main method to launch the JavaFX application.
+   *
+   * @param args the command line arguments.
+   */
   public static void main(String[] args) {
     launch(args);
   }
 
+  /**
+   * The start method is called when the JavaFX application is launched.
+   *
+   * @param primaryStage the primary stage for this application, onto which
+   *                     the application scene can be set.
+   */
   @Override
   public void start(Stage primaryStage) {
     AppInitializer appInitializer = new AppInitializer(primaryStage);
